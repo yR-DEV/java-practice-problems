ead438a66ae6f9585fd46f8fe3a68489a9127e06
Zack C <claar.zack@gmail.com>
2021-01-20 09:22:53 -0600

    Write a Java program to swap the value of two integers without creating a third variable

---
+++ SwappingXAndY.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SwappingXAndY {
+
+	public static void main(String[] args) {
+		System.out.println("Swapping");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please Enter X/1st value: ");
+		int x = scanner.nextInt();
+		System.out.println(" ");
+		System.out.println("Please Enter Y/2nd value: ");
+		int y = scanner.nextInt();
+		System.out.println(" ");
+		System.out.println("You entered values: " + x + " and " + y);
+		
+		x = x + y;   
+        y = x - y;   
+        x = x - y; 
+		
+		System.out.println("Swapped values are: " + x + " and " + y);
+	}
+
+}
