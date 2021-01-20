2f85f3f4303b84eceff80a0632b5a51c9aee72d9
Zack C <claar.zack@gmail.com>
2021-01-20 09:28:51 -0600

    Add solution for swapping values without creating a third variable

--- SwappingXAndY.java
+++ SwappingXAndY.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class SwappingXAndY {
 
 	public static void main(String[] args) {
-		System.out.println("Swapping");
 		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Swapping");
 		System.out.println("Please Enter X/1st value: ");
 		int x = scanner.nextInt();
+	
 		System.out.println(" ");
 		System.out.println("Please Enter Y/2nd value: ");
 		int y = scanner.nextInt();
+		
 		System.out.println(" ");
 		System.out.println("You entered values: " + x + " and " + y);
 		
@@ -19,5 +22,4 @@ public class SwappingXAndY {
 		
 		System.out.println("Swapped values are: " + x + " and " + y);
 	}
-
 }
