db2aa2c49923bea0623d0a392c138d6a15a23b95
Zack C <claar.zack@gmail.com>
2021-01-21 11:13:15 -0600

    Added properties, getters and setters + toString

--- ExampleClassShape.java
+++ ExampleClassShape.java
@@ -1,6 +1,48 @@
 
 public class ExampleClassShape {
-
 	
+	public String color;
+	public int sides;
+	public boolean shape = true;
+	
+	public ExampleClassShape(String color, int sides, boolean shape) {
+		this.color = color;
+		this.sides = sides;
+		this.shape = shape;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getSides() {
+		return sides;
+	}
+
+	public void setSides(int sides) {
+		this.sides = sides;
+	}
+
+	public boolean isShape() {
+		return shape;
+	}
+
+	public void setShape(boolean shape) {
+		this.shape = shape;
+	}
+
+	@Override
+	public String toString() {
+		return "ExampleClassShape [color=" + color + ", sides=" + sides + ", shape=" + shape + "]";
+	}
+	
+	public static void main(String[] args) {
+		ExampleClassShape square = new ExampleClassShape("red", 4, true);
+		System.out.println("NEW SQUARE CREATED: " + square.toString());
+	}
 	
 }
